5a3a2a3ae0920f33ddce44235acb62b6a0a2ba8e
AzamatAk <ahavov_azamat@mail.ru>
2023-06-07 21:18:11 +0300

    Fix1.3

--- src/index.js
+++ src/index.js
@@ -12,10 +12,8 @@ const startGame = (getRound, description) => {
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer !== correctAnswer) {
-      console.log(
-        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
-        `Let's try again, ${userName}!`,
-      );
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
       return;
     }
     console.log('Correct!');
